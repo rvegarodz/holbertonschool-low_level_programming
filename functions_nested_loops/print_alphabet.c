dbaec3e50282d08a0684cb99b0418fc53cd5c3d7
rvegarodz <r.vega27@outlook.com>
2022-10-05 10:50:42 -0700

    adding file

---
+++ functions_nested_loops/print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - display de alphabet in lower and upper case with putchar
+ * Return: always 0 (success)
+*/
+
+void main(void)
+{
+	int az;
+	for (az = 'a'; az <= 'z'; az++)
+		_putchar(az);
+	_putchar('\n');
+	return 0;	
+}
