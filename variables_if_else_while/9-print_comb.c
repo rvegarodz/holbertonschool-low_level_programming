a3ba4bb6237187665ea1b9878d6663e4f200bc1b
rvegarodz <r.vega27@outlook.com>
2022-10-05 08:46:59 -0700

    adding file 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,12 +8,15 @@
 int main(void)
 {
 	int n;
-	for (n = '0'; n <= '9'; n++)	
-		{
-			putchar(n);
-			putchar(',');
-			putchar(' ');
-		}
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
+	
 	return 0;	
 }
