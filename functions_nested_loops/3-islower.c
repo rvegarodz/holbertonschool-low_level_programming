a538870ee65d225f690a9691b375044f02f074a5
rvegarodz <r.vega27@outlook.com>
2022-10-05 12:27:48 -0700

    adding file

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -9,15 +9,13 @@
 
 int _islower(int c)
 {
-	if(c >= 'a' && c >= 'z')
+	if(c >= 'a' && c <= 'z')
 	{
-		_putchar('1');
+		return (1);
 	}
 	else
 	{
-		_putchar('0');
+		return (0);
 	}
-	
-	return 0;
 		
 }
