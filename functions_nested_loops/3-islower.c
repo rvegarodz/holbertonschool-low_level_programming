f6f0022920c746d3a5ec958011fdfc6cbfa9dff2
rvegarodz <r.vega27@outlook.com>
2022-10-05 12:40:55 -0700

    adding file

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/*
+/**
  * _islower - print for lowercase characters
- *
+ * @c: c is any lowercase letter
  * Return: always 0 (success)
-*/
+ */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
