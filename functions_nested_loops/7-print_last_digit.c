d81146b32354b0c9cee549f6e8c27e6c80656c1a
rvegarodz <r.vega27@outlook.com>
2022-10-05 14:50:21 -0700

    adding file

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,7 @@ int print_last_digit(int x)
 {
 	int digit;
 
-	if (x < 0)
-	{
-		x = x * -1
-		digit = x % 10;
-		return (digit);
-	}
-	else
-	{
-		digit = x % 10;
-		return (digit);
-	}
+	digit = x % 10;
+	return (digit);
+	
 }
