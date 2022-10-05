de90e96059b31fc10db2d931c84037ca2e721580
rvegarodz <r.vega27@outlook.com>
2022-10-05 13:05:21 -0700

    adding file

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- *
+ * @n: n is any number
  * Return: 1 for positive, 0 for zero and -1 for negative
  */
 int print_sign(int n)
