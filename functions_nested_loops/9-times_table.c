925842545d4cc6c01b9b8b70ea882fe00d8fe2ea
rvegarodz <r.vega27@outlook.com>
2022-10-05 19:24:06 -0700

    adding file

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * 
+ *
+ *
+ */
+void times_table(void)
+{
+	init x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+
+		for (y = 0; y <= 9; y++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			z = x * y;
+
+			if (z <= 9)
+				_putchar(' ');
+			else
+				_putchar('0' + (z / 10));
+
+			_putchar('0' + (z % 10));
+		}
+		_putchar(''\n');
+	}
+}
