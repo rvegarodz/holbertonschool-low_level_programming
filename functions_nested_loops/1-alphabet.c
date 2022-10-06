ac32f020950eeb3e1fe996db433760de6306c347
rvegarodz <r.vega27@outlook.com>
2022-10-05 20:51:46 -0700

    edited

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,13 @@
 #include "main.h"
-
 /**
  * print_alphabet - display de alphabet in lower and upper case with putchar
- * 
  * Return: always 0 (success)
  *
 */
-
 void print_alphabet(void)
-
 {
 	int az;
-	
+
 	for (az = 'a'; az <= 'z'; az++)
 		_putchar(az);
 	_putchar('\n');
