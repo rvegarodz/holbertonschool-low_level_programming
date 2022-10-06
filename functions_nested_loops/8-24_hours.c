b24a7875e80156826fda44a8dedfb215377877f1
rvegarodz <r.vega27@outlook.com>
2022-10-05 18:46:04 -0700

    adding file

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,10 +9,10 @@ void jack_bauer(void)
 {
 	int hora0, hora1, sec0, sec1;
 
-	_putchart('0' + hora0);
-	_putchart('0' + hora1);
-	_putchart(':');
-	_putchart('0' + sec0);
-	_putchart('0' + sec1);
-	_putchart('\n');
+	_putchar('0' + hora0);
+	_putchar('0' + hora1);
+	_putchar(':');
+	_putchar('0' + sec0);
+	_putchar('0' + sec1);
+	_putchar('\n');
 }
