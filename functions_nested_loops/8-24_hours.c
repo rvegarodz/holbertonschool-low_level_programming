51f741ecff551f986555cb52a249e7714923e3ec
rvegarodz <r.vega27@outlook.com>
2022-10-05 19:11:01 -0700

    adding file

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,11 +14,11 @@ void jack_bauer(void)
 		for (min = 0; min <= 59; min++)
 		{
 		_putchar('0' + (hr / 10));
-		_putchar('0' + (hr % 10);
+		_putchar('0' + (hr % 10));
 		_putchar(':');
 		_putchar('0' + (min / 10));
 		_putchar('0' + (min % 10));
-		_putchar('\n');;
+		_putchar('\n');
 		}
 	}
 }
