288abb319f6000d5b93733d161ea9a0099b26df8
rvegarodz <r.vega27@outlook.com>
2022-10-07 16:51:05 -0700

    adding file

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,6 +7,7 @@
 void print_numbers(void)
 {
 	int num;
+	
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0' + num);
---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers -
+ * x
+ * Return:
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 || num != 4)
+			return (num);
+	}
+}
