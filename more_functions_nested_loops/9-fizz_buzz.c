3b74f52601dc41f9dabde5c23bfd3d9e6538b829
rvegarodz <r.vega27@outlook.com>
2022-10-07 20:07:03 -0700

    adding file 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,14 @@ void fizz_buzz(int num)
 	for (i = 1; i <= num; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		_putchar(' ');
 	}
+	_putchar('\n');
 }
