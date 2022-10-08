7075f0fa602158801304e4eedd2fc84ec86ca741
rvegarodz <r.vega27@outlook.com>
2022-10-08 10:42:42 -0700

    addign file 10 test

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle
+ * size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (size == i)
+	{
+		_putchar('#');
+	}
+		
+	else
+	{
+		for (i = 1, i <= size; i++)
+		{
+			if (size == i)
+			{
+				_putchar('#');
+			}
+			for (j = 1; j < size; j++)
+			{
+				for (z = 1; z = j; z++)
+				{
+					_putchar('#');
+				}
+				_putchar(' ');
+			}
+			_putchar('\n');
+	}
+}
