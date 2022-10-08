84343bc62396e2182c9c0487fbb2677a5e16ff0a
rvegarodz <r.vega27@outlook.com>
2022-10-07 18:27:54 -0700

    adding file 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - print hashtag
+ * @i: num of print by line
+ * @j: num of print
+ */
+void print_hashtag(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size, i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
