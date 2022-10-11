3a45e9cf57f41373ff5c2ddeaff0c239fd09aa04
rvegarodz <r.vega27@outlook.com>
2022-10-11 16:47:44 -0700

    adding

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,16 @@ void rev_string(char *s)
 {
 	int i = 0, j = 0, z = 0;
 	int string_length = i;
-
+	*s = &s;
+	
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	j = i - 1;
 	
-	char rev['string_lenght'] = *s;
-	
+	char rev[] = *s;
+
 	while (j >= 0)
 	{
 		rev[z] = s[j];
