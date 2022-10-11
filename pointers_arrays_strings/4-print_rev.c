5a0835203ae690d2f2d2daa6c8afed80b2f8892f
rvegarodz <r.vega27@outlook.com>
2022-10-11 12:25:54 -0700

    adding file

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int count = 0;
+	int count = 0, i = 0;
 
 	while (s[count] != '\0')
 	{
 		count++;
 	}
-	while (s[count] == 0)
+	i == count;
+	while (s[i] == 0)
 	{
-		_putchar(s[count]);
-		count--;
+		_putchar(s[i);
+		i--;
 	}
 	_putchar('\n');
 }
