053d13d911ebb33a692f48dfad2e36285582a202
rvegarodz <r.vega27@outlook.com>
2022-10-12 21:26:55 +0000

    adding file 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void print_array(int *a, int n)
+{
+	int y;
+
+	for (y = 0; y <= 0; y++)
+	{
+		if (y < n)
+		{
+			printf("%d", a[y]);
+			printf(", ");
+		}
+		else (y = n)
+		{
+			printf("%d\n", a[y]);
+		}
+	}
+
+}
