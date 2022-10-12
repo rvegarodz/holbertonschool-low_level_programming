f1541991174348dfbee121b5c07c9062acf6fd4d
rvegarodz <r.vega27@outlook.com>
2022-10-12 23:58:40 +0000

    updated readme file

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * print_array - print n elements of any array
+ * @a: pointer to array
+ * @n: number of elements of the arra
  */
 void print_array(int *a, int n)
 {
