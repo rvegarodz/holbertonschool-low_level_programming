86ac11ff686557bfad064542b2386d690e41be45
rvegarodz <r.vega27@outlook.com>
2022-10-12 07:30:11 -0700

    adding file 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
 void rev_string(char *s)
 {
 	int i = 0, j = 0, z = 0;
-	char *s = &s;
+	int temp;
 	
 	while (s[i] != '\0')
 	{
@@ -15,20 +15,12 @@ void rev_string(char *s)
 	}
 	j = i - 1;
 	
-	char rev[] = *s;
 
-	while (j >= 0)
+	while (z < 0)
 	{
-		rev[z] = s[j];
-		z++;
-		j--;
-	}
-	
-	while (z >= 0)
-	{
-		s[j] = rev[z];
-		j++;
-		z--;
+		temp = s[z];
+		s[z] = s[j];
+		s[j] = temp
 	}
 	_putchar('\n');
 }
