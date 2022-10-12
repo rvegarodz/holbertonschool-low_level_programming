e0aa1a3cbdd2b64468e8310c5950638db58498cd
rvegarodz <r.vega27@outlook.com>
2022-10-12 23:43:31 +0000

    updated readme file

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ * Return: return count (success)
  */
 int _strlen(char *s)
 {
-	int count = 0, i = 0;
-	
+	int count = 0, i = 0;	
 	while (s[i] != '\0')
 	{
 		i++;
