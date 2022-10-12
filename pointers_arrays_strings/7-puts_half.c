cb2b386d3533231f26db55af3ae891e9ac8e440e
rvegarodz <r.vega27@outlook.com>
2022-10-12 19:35:31 +0000

    addign file 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,7 @@ void puts_half(char *str)
 	
 	if (len % 2 == 0)
 	{
-		hlf = len / 2;
-		for (str[hlf] <= len; hlf++)
+		for (hlf = len / 2; str[hlf] <= len; hlf++)
 		{
 			_putchar(str[hlf]);
 		}
