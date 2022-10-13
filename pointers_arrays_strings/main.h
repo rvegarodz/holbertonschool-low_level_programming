129c591a691047986bf5dbd4b72a9b65442e1ca5
rvegarodz <r.vega27@outlook.com>
2022-10-13 00:42:21 +0000

    adding file 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,4 +8,4 @@ void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcat(char *dest, char *src);
-char *_strcat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
