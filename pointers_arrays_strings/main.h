92a59a7a5a05336dc1005ed7b94ef875b9a206af
rvegarodz <r.vega27@outlook.com>
2022-10-13 15:23:32 +0000

    adding file 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ void print_array(int *a, int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *string_toupper(char *s);
+char *_memset(char *s, char b, unsigned int n);
