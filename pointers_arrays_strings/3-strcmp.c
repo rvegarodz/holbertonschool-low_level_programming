6ec515a8d5086dce9593d14460bc9b81915e8802
rvegarodz <r.vega27@outlook.com>
2022-10-13 01:49:10 +0000

    adding file 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,21 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int flag, i;
-
-	flag = 0;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 == *s2)
 		{
-			flag = 1;
+			retunr (0);
 		}
+		s1++;
+		s2++
 	}
-	if (s1[i] != '\0' || s2[i] != '\0')
-		return 1;
-	if (flag == 0)
-		return (0);
-	else
-		return (1);
+	return (*s1 - *s2);
 }
