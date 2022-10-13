b0052f22c2688b019d19f6e16fc7b842540c84fc
rvegarodz <r.vega27@outlook.com>
2022-10-13 17:59:39 +0000

    adding file 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -6,18 +6,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int ln = 0;
 	unsigned int i;
-
-	while (dest[ln] != '\0')
-	{
-		ln++;
-	}
 
 	for (i = 0; i < n; i++)
 	{
 		dest[ln] = src[i];
-		ln++;
+		i++;
 	}
 	return (dest);
 }
---
+++ pointers_arrays_strings/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int array[4] = {1, 2, 3, 4};
+	printf("%d\n", array[4]);
+	return (0);
+}
