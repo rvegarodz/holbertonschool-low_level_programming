fffd1b935e0b1f60e49391665f599e7b875699ea
rvegarodz <r.vega27@outlook.com>
2022-10-14 16:38:24 +0000

    adding file 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, count;
+
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			       count++;
+			else
+				j++;	
+		}
+	}
+	return (0);
+}
