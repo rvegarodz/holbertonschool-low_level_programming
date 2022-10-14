7ca0d217a76ff13a722dcd920b1097b588943141
rvegarodz <r.vega27@outlook.com>
2022-10-14 14:33:04 +0000

    adding file 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			_putchar(s[i]);
+		}
+		else if (s[i] != c)
+		{
+			_putchar('N');
+			_putchar('U');
+			_putchar('L');
+			_putchar('L')
+		}
+	}
+	return (0);
+}
