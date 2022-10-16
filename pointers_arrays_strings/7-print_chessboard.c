2bc889839c7514e1fb903c75dc771ae11751353b
rvegarodz <r.vega27@outlook.com>
2022-10-16 20:38:44 +0000

    adding file 6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 	i = 0;
 	j = 0;
 
-	for ( i = 0; i <= 8; i++)
+	for ( i = 0; i != '\0'; i++)
 	{
-		for ( j = 0; j <= 8; j++)
+		for ( j = 0; j != 0; j++)
 		{
 			_putchar(a[i][j]);
 		}
