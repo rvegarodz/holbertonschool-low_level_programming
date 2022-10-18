fc4807b05580b050c67425ba9451a5bd8380dd47
rvegarodz <r.vega27@outlook.com>
2022-10-18 21:55:40 +0000

    adding file with betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power value
+ * Return: value of x to power y (success)
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+		return (-1);
 	else if (y == 0)
-		return 1;
+		return (1);
 	else if (x == 0)
-		return 0;
+		return (0);
 	else
 		return (x * _pow_recursion(x, y - 1));
 }
