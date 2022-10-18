833a84c381e77b187d02bbfef3be1f0d2f2c380c
rvegarodz <r.vega27@outlook.com>
2022-10-18 20:30:00 +0000

    adding file

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,13 +10,14 @@ int is_prime_number(int n)
 {
 	return (primes(n, 2));
 }
+
 int primes(int n, int i)
 {
 	if (n == 0 || n == 1)
 		return (0);
-	else if (n == i)
+	if (n == i)
 		return (1);
-	else if (n % i == 0)
+	if (n % i == 0)
 		return (0);
 	i++;
 	return is_prime_number(n);
