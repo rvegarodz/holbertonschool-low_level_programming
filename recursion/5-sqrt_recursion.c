f2ba7e98380a9e00143c9fd5301f370e2d131570
rvegarodz <r.vega27@outlook.com>
2022-10-18 22:07:00 +0000

    adding file with betty

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,22 +1,23 @@
 #include "main.h"
 /**
  * _sqrt_recursion - return the natural square root of a number
- * _sqrt_root - square root of a number
- * @root: root value of n
- * @n: value to analize
- * Return: square root (success)
- */
+ */ 
 int sqrt_root(int root, int n);
 
 int _sqrt_recursion(int n)
 {
 	return (sqrt_root(0, n));
 }
-
-int sqrt_root(int root, int n)
+/**
+ * sqrt_root - root of a number
+ * @root: root value of n
+ * @n: value to analize
+ * Return: square root (success)
+ */
+ int sqrt_root(int root, int n)
 {
 	int sqr = root * root;
-	
+
 	if (sqr < n)
 		return (sqrt_root(root + 1, n));
 	else if (sqr > n)
