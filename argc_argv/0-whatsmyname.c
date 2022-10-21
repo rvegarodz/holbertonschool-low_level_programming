3efec67036fcd111180bfdf7782c2b0d0f5f6aac
rvegarodz <r.vega27@outlook.com>
2022-10-21 00:29:43 +0000

    adding file

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,9 @@
 #include "stdio.h"
 /**
- *
- *
- *
+ * main - print program name
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: always 0 (success)
  */
 int main(int argc, char *argv[])
 {
