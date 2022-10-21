0a1a1cc8e7f2aa597fdad84f7d85a83241b2e4ff
rvegarodz <r.vega27@outlook.com>
2022-10-21 12:21:30 +0000

    adding file 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+ * main - print number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: unused
+ * Return: always 0 (success)
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+
+	printf("%d\n", argc);
+	
+	return (0);
+}
