1a8b823722659ef24288427ae40c549ce1bdc8e7
rvegarodz <r.vega27@outlook.com>
2022-10-21 13:20:07 +0000

    adding file

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print program name
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int x, y, mult;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+	       	x = atoi(argv[1]);
+		y = atoi(argv[2]);
+
+		mult = (x * y);
+
+		printf("%d\n", mult);
+	}
+	return (0);
+}
