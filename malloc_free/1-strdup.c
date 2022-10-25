c6ab79bf7313e921381935126c75d67ae0979a2f
rvegarodz <r.vega27@outlook.com>
2022-10-25 20:28:23 +0000

    adding file 1 edited

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,15 +2,26 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * _strdup - pointer to copy of string
+ * @str: string to copy
+ * Return: array (success)
+ */
 char *_strdup(char *str)
 {
 	char *array;
 	unsigned int i;
 	unsigned int length = strlen(str);
-	
-	array = malloc( length );
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	array = malloc(sizeof(char) *(length+1));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < length; i++)
 	{
 		array[i] = str[i];
