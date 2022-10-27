7b8915cb98295323c1c0ee399817c1f1deb9a8c0
rvegarodz <r.vega27@outlook.com>
2022-10-27 22:54:29 +0000

    adding file 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 		len2 = strlen(s2);
 	if (n >= len2)
-		n = len2;	
+		n = len2;
 	ptr = malloc(sizeof(char) * (len1 + n + 1));
 	if (ptr == NULL)
 		return (NULL);
