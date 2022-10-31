ec926b99e1c144a502a532a25943384ba40f3ab6
rvegarodz <r.vega27@outlook.com>
2022-10-31 17:33:42 +0000

    adding file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,25 +1,6 @@
-#include <stdlib.h>
-/**
- *
- *
- */
-
-struct Dog
+struct dog
 {
 	char *name;
 	float age;
-	char *owner;
+	char *owner; 
 };
-
-struct Dog *my_dog(char *name; float age; char *owner)
-{
-	struct Dog *dog;
-
-	dog = malloc(sizeof(struct Dog));
-	if (dog == NULL)
-		return (NULL);
-	dog->name = name;
-	dog->age = age;
-	dog-> owner = owner;
-	return (dog);
-}
