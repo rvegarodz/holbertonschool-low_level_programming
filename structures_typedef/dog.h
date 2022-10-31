b2324567ab34421398c23c0b36cb0b2a1c91072b
rvegarodz <r.vega27@outlook.com>
2022-10-31 17:46:20 +0000

    adding file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - define a new struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char *owner; 
+	char *owner;
 };
-
 #endif
