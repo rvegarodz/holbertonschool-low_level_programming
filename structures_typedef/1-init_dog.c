a395ef5259a1a6a92bd23e5587e17427b6344b9d
rvegarodz <r.vega27@outlook.com>
2022-10-31 18:17:28 +0000

    adding file

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - function that initialize a struct dog
+ * @d: pointer to struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *ptr;
+
+	ptr = d;
+	/**d == malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	*/
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+}
