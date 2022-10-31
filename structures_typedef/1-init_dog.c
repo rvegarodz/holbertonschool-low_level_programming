1601faa15d1b9269dc92883abb1918038ff070b0
rvegarodz <r.vega27@outlook.com>
2022-10-31 18:30:28 +0000

    adding file

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	struct dog *ptr;
 	ptr = d;
 	if (d == NULL)
-		return (void);
+		exit (void);
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
