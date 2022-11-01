1da89429fa037cafae245bb4ef3c7a142b9a5dcb
rvegarodz <r.vega27@outlook.com>
2022-11-01 17:23:09 +0000

    adding file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,23 +15,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-		return (NULL);
-	
+		return(NULL);	
 	new_dog->name = malloc(sizeof(name) * strlen(name));
 	if (new_dog->name == NULL)
 	{
-		free (new_dog);
-		return (NULL);
+		free(new_dog);
+		return(NULL);
 	}
 	new_dog->owner = malloc(sizeof(owner) * strlen(owner));
 	if (new_dog->owner == NULL)
 	{
-		free (new_dog->name);
-		free (new_dog);
-		return (NULL);
+		free(new_dog->name);
+		free(new_dog);
+		return(NULL);
 	}
 	strcpy(new_dog->name, name);
 	new_dog->age = age;
 	strcpy(new_dog->owner, owner);
-	return (new_dog);
+	return(new_dog);
 }
