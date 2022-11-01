f7cad04d104054b1e5304c1122df0c7a7e267204
rvegarodz <r.vega27@outlook.com>
2022-11-01 19:21:02 +0000

    adding file

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
- *
+ * print_name - print name using pointer to function
+ * @name: pointer to string to print
+ * @f: function to print
  */
 void print_name(char *name, void (*f)(char *))
 {
