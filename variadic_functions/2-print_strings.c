8e906eb439fd26167ef10472f722ff63639ee42f
rvegarodz <r.vega27@outlook.com>
2022-11-03 17:36:07 +0000

    adding file

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,13 +11,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *j;
 	va_list ptr;
+	
 	va_start(ptr, n);
-
 	if (separator != NULL)
 		for (i = 0; i < n; i++)
 		{
 			j = va_arg(ptr, char *);
-			if (j != NULL)	
+			if (j != NULL)
 				printf("%s", j);
 			else
 				printf("(nil)\n");
