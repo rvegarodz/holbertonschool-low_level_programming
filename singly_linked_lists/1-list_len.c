8efa2e1d200a7cd14c7dbc483f926b8f43c58633
rvegarodz <r.vega27@outlook.com>
2022-11-15 22:58:41 +0000

    editing betty style of 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * list_len - function that returns the number of elements in list_t
+ * @h: pointer to head node
+ * Return: number of nodes of list_t (success)
  */
 size_t list_len(const list_t *h)
 {
