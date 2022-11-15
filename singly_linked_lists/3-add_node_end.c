922fad3ffbf8c20c913c029d4f92f73f57c74f59
rvegarodz <r.vega27@outlook.com>
2022-11-15 23:59:22 +0000

    editing pointer to head

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -23,7 +23,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = newnode;
-		return (head);
+		return (*head);
 	}
 	last = *head;
 	while (last->next != NULL)
