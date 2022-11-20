98b767883afa643167f55f64d5bd0d016a3bc5e5
rvegarodz <r.vega27@outlook.com>
2022-11-20 17:06:54 +0000

    correcting edge case and betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * create_file - function that creates a file
- * filename - name of the file to create
- * text_content - text to include in the file
+ * @filename: name of the file to create
+ * @text_content: text to include in the file
  * Return: 1 if (success) or -1 on (failure)
  */
 int create_file(const char *filename, char *text_content)
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1 || filename == NULL)
 		return (-1);
 	if (text_content != NULL)
-		write (fd, text_content, len);
+		write(fd, text_content, len);
 	close(fd);
 	return (1);
 }
