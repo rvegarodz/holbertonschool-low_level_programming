5ada4f98d9479ed38a0796926aa1b2367add0282
rvegarodz <r.vega27@outlook.com>
2022-11-20 17:54:19 +0000

    edge case edited

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,15 +10,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, len = 0;
 
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	if (filename == -1 || fd == -1)
 	{
-		return (0);
+		return (-1);
 	}
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
-		write(fd, text_content, len);
 	}
+	write(fd, text_content, len);
 	close(fd);
 	return (1);
 }
