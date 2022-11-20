d5f7ad763cfaa92de4ac2a6e4d86c471fb4fea2d
rvegarodz <r.vega27@outlook.com>
2022-11-20 17:04:41 +0000

    correcting edge case and betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,20 +10,15 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int len;
 
-	len = strlen(text_content);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1 || filename == NULL)
-	{
-		return (-1);
-	}
 	if (text_content != NULL)
-	{
-		write (fd, text_content, len);
-	}
+		len = strlen(text_content);
 	else
-		write (fd, '\0', 1);
+		len = 0;
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1 || filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		write (fd, text_content, len);
 	close(fd);
 	return (1);
-	
 }
