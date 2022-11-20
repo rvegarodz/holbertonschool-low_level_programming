238332f83a326fb3a5a5dcec0c498a7be58d949b
rvegarodz <r.vega27@outlook.com>
2022-11-20 16:45:05 +0000

    edit edge case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,7 +6,7 @@ int create_file(const char *filename, char *text_content)
 	int len;
 
 	len = strlen(text_content);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
