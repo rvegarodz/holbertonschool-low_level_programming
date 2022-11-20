4ddfc039f72103ac2b5e5c0eb01360a1c097f655
rvegarodz <r.vega27@outlook.com>
2022-11-20 16:57:56 +0000

    edit edge case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * create_file - function that creates a file
+ * filename - name of the file to create
+ * text_content - text to include in the file
+ * Return: 1 if (success) or -1 on (failure)
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -8,11 +13,16 @@ int create_file(const char *filename, char *text_content)
 	len = strlen(text_content);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (fd == -1 || filename == NULL)
 	{
 		return (-1);
 	}
-	write (fd, text_content, len);
+	if (text_content != NULL)
+	{
+		write (fd, text_content, len);
+	}
+	else
+		write (fd, '\0', 1);
 	close(fd);
 	return (1);
 	
