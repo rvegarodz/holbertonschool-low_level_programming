c0986f990e66547a89b864c983286fe1bdd851d6
rvegarodz <r.vega27@outlook.com>
2022-11-21 19:40:23 +0000

    adding file for task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - function that returns the number of elements in a linked dlistint_t list
+ * @h: pointer to head
+ * Return: number of nodes (success)
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
