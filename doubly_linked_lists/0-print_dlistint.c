40b7166fd30a5ea1b64c657a62ebe35c9f8b3ad8
rvegarodz <r.vega27@outlook.com>
2022-11-21 19:32:51 +0000

    editing betty style

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * 
- *
- *
+ * print_dlistint - function that prints all the elements of a dlistint_t list 
+ * @h: pointer to head
+ * Return: number of nodes (success)
  */
 size_t print_dlistint(const dlistint_t *h)
 {
