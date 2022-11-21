e1280a68990ec28e748d1356b0349c80314486db
rvegarodz <r.vega27@outlook.com>
2022-11-21 20:20:55 +0000

    adding file for task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	newnode = malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->prev = NULL;
+	newnode->next = *head;
+	if (*head != NULL)
+		(*head)->prev = newnode;
+	*head = newnode;
+	return (newnode);
+}
