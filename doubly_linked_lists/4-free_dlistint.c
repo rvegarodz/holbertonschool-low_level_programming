2dee7f8ddb8aab5a680d23c4223cd0edad39360d
rvegarodz <r.vega27@outlook.com>
2022-11-22 15:29:50 +0000

    adding file for task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dnodeint - function that frees a list
+ * @head: pointer to first node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tailnode = head;
+
+	if (tailnode->next == NULL)
+	{
+		free(head);
+	}
+	while (tailnode->next != NULL)
+	{
+		tailnode = tailnode->next;
+		free(head);
+		head = tailnode;
+	}
+	free(head);
+}
