57ee97429e87b95017b6305b47bc04b9f735dadd
rvegarodz <r.vega27@outlook.com>
2022-11-22 20:00:18 +0000

    editing betty

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 /**
- *
- *
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to first node
+ * @idx: position to add
+ * @n: value to add
+ * Return: address of the new node (success) or NULL (failed)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
